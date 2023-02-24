b45e3f2c1190fa8766e5bc8f7a7cda437727fbd8
unknown <ogunkapc@gmail.com>
2023-02-24 19:02:34 +0100

    Updated dependencies and modified code

--- lib/todo.dart
+++ lib/todo.dart
@@ -4,158 +4,108 @@ class TodoApp extends StatefulWidget {
   const TodoApp({Key? key}) : super(key: key);
 
   @override
-  _TodoAppState createState() => _TodoAppState();
+  State<TodoApp> createState() => _TodoAppState();
 }
 
 class _TodoAppState extends State<TodoApp> {
-  //todo list items
+  //! List of todo items
   final List _todoItems = [];
-
-  // controller to help us track the changes in our text field
+  //! controller to help track changes in textfield
   final textController = TextEditingController();
 
-  // Function to add todo items
-
-  addTodo(String task) {
-    if (task.isNotEmpty) {
+  //! Function to add todo item
+  addTodo(String todoItem) {
+    if (todoItem.isNotEmpty) {
       setState(() {
-        _todoItems.add(task);
+        _todoItems.add(todoItem);
       });
     }
   }
 
+  //! Function to delete todo item
   deleteTodo(int index) {
     setState(() {
       _todoItems.removeAt(index);
     });
   }
-  // onDispose() {
-  //   // clears the text field
-  //   textController.dispose();
-  //
-  //   super.dispose();
-  // }
-  // onInit(){
-  //
-  //   // Perform operation on start of a screen
-  //   super.initState();
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-// floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (context) => Container(
-                    padding: const EdgeInsets.all(24),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: textController,
-                          onChanged: (value) {
-                            print(value);
-                            print(textController.text);
-                          },
-                          decoration: const InputDecoration(
-                              label: Text("Enter your todo here"),
-                              labelStyle: TextStyle(
-                                  color: Colors.indigo, fontSize: 18)),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            addTodo(textController.text);
-                            textController.clear();
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Add Todo"),
-                        )
-                      ],
-                    ),
-                  ));
-        },
-        child: const Icon(Icons.add, color: Colors.white),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: const Text(
-          "Todoly",
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          title: const Text(
+            "My Schedule",
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            const FlutterLogo(
-              size: 30,
-            ),
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                itemCount: _todoItems.length,
-                itemBuilder: (context, index) => Card(
-                  child: ListTile(
-                    title: GestureDetector(
-                        onTap: () {
-                          showModalBottomSheet(
-                            context: context,
-                            builder: (context) => Container(
-                              padding: const EdgeInsets.all(24),
-                              child: Text(
-                                _todoItems[index],
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: _todoItems.length,
+                  itemBuilder: (context, index) => Card(
+                    child: ListTile(
+                      title: GestureDetector(
+                          onTap: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) => Container(
+                                padding: const EdgeInsets.all(24),
+                                child: Text(
+                                  _todoItems[index],
+                                ),
                               ),
-                            ),
-                          );
+                            );
+                          },
+                          child: Text(_todoItems[index])),
+                      trailing: IconButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) => Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 24, vertical: 50),
+                                    color: Colors.white,
+                                    height:
+                                        MediaQuery.of(context).size.height / 3,
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const Text(
+                                            "Are you Sure you want to delete?"),
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            deleteTodo(index);
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text("Delete Todo"),
+                                        ),
+                                      ],
+                                    ),
+                                  ));
                         },
-                        child: Text(_todoItems[index])),
-                    trailing: IconButton(
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) => Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 24, vertical: 50),
-                                  color: Colors.white,
-                                  height:
-                                      MediaQuery.of(context).size.height / 3,
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      const Text(
-                                          "Are you Sure you want to delete?"),
-                                      ElevatedButton(
-                                        onPressed: () {
-                                          deleteTodo(index);
-                                          Navigator.pop(context);
-                                        },
-                                        child: const Text("Delete Todo"),
-                                      ),
-                                    ],
-                                  ),
-                                ));
-                      },
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.red,
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          // backgroundColor: Colors.indigo,
+          onPressed: () {},
+          child: const Icon(Icons.add, color: Colors.white),
+        ));
   }
 }
